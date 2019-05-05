71738ac39656eb6a00097bdab6a34f7f27a5dd39
Vladyslav Hnatiuk <smthnghere@gmail.com>
2019-05-05 18:26:55 +0300

    Deleted useless items

--- Coursework in Java/Controllers/PostsController.cs
+++ Coursework in Java/Controllers/PostsController.cs
@@ -21,20 +21,6 @@ namespace Coursework_in_Java.Controllers
             return View(await db.Posts.ToListAsync());
         }
 
-        // GET: Posts/Details/5
-        public async Task<ActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Posts posts = await db.Posts.FindAsync(id);
-            if (posts == null)
-            {
-                return HttpNotFound();
-            }
-            return View(posts);
-        }
 
         // GET: Posts/Create
         public ActionResult Create()
@@ -43,8 +29,6 @@ namespace Coursework_in_Java.Controllers
         }
 
         // POST: Posts/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<ActionResult> Create([Bind(Include = "Id,Title,Content")] Posts posts)
@@ -57,63 +41,6 @@ namespace Coursework_in_Java.Controllers
             }
 
             return View(posts);
-        }
-
-        // GET: Posts/Edit/5
-        public async Task<ActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Posts posts = await db.Posts.FindAsync(id);
-            if (posts == null)
-            {
-                return HttpNotFound();
-            }
-            return View(posts);
-        }
-
-        // POST: Posts/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Edit([Bind(Include = "Id,Title,Content")] Posts posts)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(posts).State = EntityState.Modified;
-                await db.SaveChangesAsync();
-                return RedirectToAction("Index");
-            }
-            return View(posts);
-        }
-
-        // GET: Posts/Delete/5
-        public async Task<ActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Posts posts = await db.Posts.FindAsync(id);
-            if (posts == null)
-            {
-                return HttpNotFound();
-            }
-            return View(posts);
-        }
-
-        // POST: Posts/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<ActionResult> DeleteConfirmed(int id)
-        {
-            Posts posts = await db.Posts.FindAsync(id);
-            db.Posts.Remove(posts);
-            await db.SaveChangesAsync();
-            return RedirectToAction("Index");
         }
 
         protected override void Dispose(bool disposing)
